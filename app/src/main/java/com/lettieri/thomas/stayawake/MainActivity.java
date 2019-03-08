a1932546db1da89c15f24d7f8068845c4cb60223
Thomas Lettieri <thomasl@ips-yes.com>
2019-03-08 09:31:33 -0500

    Initial commit

---
+++ app/src/main/java/com/lettieri/thomas/stayawake/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lettieri.thomas.stayawake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
